f67e3e4831a08e9c7da8dc8d394ae20bf0655764
John M. Wargo <johnwargo@users.noreply.github.com>
2023-06-14 13:53:34 -0400

    Update pub.mjs

--- pub.mjs
+++ pub.mjs
@@ -1,7 +1,8 @@
 #!/usr/bin/env zx
 
 await $`npm version patch`;
-await $`git commit -m "Incrementing package version"`;
 await $`git push`;
+// await $`git commit -m "Incrementing package version"`;
+// await $`git push`;
 await $`npm publish`;
 // await $`git push`;
